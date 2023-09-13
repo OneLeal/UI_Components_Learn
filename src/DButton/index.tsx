2dbf9db3f44cd7405391e0b72ba4fdf5bdd52edd
浅芷初夏 <55690294+OneLeal@users.noreply.github.com>
2023-09-13 23:59:12 +0800

    feat: add button component

---
+++ src/DButton/index.tsx
@@ -0,0 +1,22 @@
+import React, { ReactNode } from "react";
+import classNames from "classnames";
+import "./style.css";
+
+// TODO: disabled & size & icon & click event
+interface IProps {
+  type?: "primary";
+  className?: string;
+  children?: ReactNode;
+}
+
+// FIXME: 外层传入的 className 优先级问题 & 原生属性透传问题
+const DButton: React.FC<IProps> = ({ type, className = "", children }) => {
+  const classNameStr = classNames({
+    "dao-btn": true,
+    [`dao-btn-${type}`]: !!type,
+    [className]: !!className,
+  });
+  return <button className={classNameStr}>{children}</button>;
+};
+
+export default DButton;
--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import DButton from './DButton';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <DButton type='primary'>Primary</DButton>
+    <DButton className='my-btn'>Default</DButton>
   </React.StrictMode>
 );
 
